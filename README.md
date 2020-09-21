a534cc3d322303a40a2eb52b244c1c199f613eea
Eshan dhawan <eshandhawan51@gmail.com>
2020-09-21 19:09:24 +0530

    speech to text conversion module

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # isl_convertor
 This repo is for minor project 1 odd sem 2020
+
+Dependencies required :
+
+pip install pyaudio
+pip install speech_recognition
+
---
+++ text_speechmodule/speechtotext.py
@@ -0,0 +1,23 @@
+#import packages
+
+import speech_recognition as sr
+
+#class for speech recognization
+
+__all__=['speechtotext']
+
+class speechtotext:
+    def __init__(self):
+        #creating the object of Recognizer class
+        # provided by speech_recognition module
+        self.recognizer=sr.Recognizer() 
+    def input_speech(self):
+        microphone = sr.Microphone()
+        with microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            audio = self.recognizer.listen(source)
+            try:
+                textoutput=self.recognizer.recognize_google(audio)
+                return(textoutput)
+            except:
+                return(textoutput)
