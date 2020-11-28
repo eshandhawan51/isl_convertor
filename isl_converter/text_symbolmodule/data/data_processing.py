59b5d92a295219c1f687a07b4068a1e6ddd4d462
Eshan Dhawan <eshandhawan51@gmail.com>
2020-11-28 12:59:41 +0530

    data processing

--- isl_converter/text_symbolmodule/data/data_processing.py
+++ isl_converter/text_symbolmodule/data/data_processing.py
@@ -1,25 +1,37 @@
 import os 
 import sys
 import cv2
+import csv
 import numpy as np
 import image_transformation as image_processing
 import matplotlib.pyplot as plt
 
 folder = ["train","test"]
 letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+for i in folder :
+    output_path = open("./{}.csv".format(i),'w')
+    writer = csv.writer(output_path,delimiter=',')
+    print("generating {}ing data".format(i))
+    for alphabets in letters:
+        path =os.path.join("./dataset",i,alphabets)
+        directory=r'{}'.format(path)
+    
+        try:
+            for filename in os.listdir(directory):
+                if filename.endswith(".jpg") or filename.endswith(".png"):
+                    image_file=os.path.join(path, filename)
+                    frame = cv2.imread(image_file)
+    #                cv2.imshow("Original", frame)
+                    try:
+                        frame=image_processing.apply_image_transformation(frame)
+                        flattened_image = frame.flatten()
+                        output_image = [alphabets]+ np.array(flattened_image).tolist()
+                        writer.writerow(output_image)
+                    except Exception :
+                        print ("failed to apply image transformation")
+                        continue
 
-
-for alphabets in letters:
-    path =os.path.join("./dataset","train",alphabets)
-    directory=r'{}'.format(path)
-    try:
-        for filename in os.listdir(directory):
-            if filename.endswith(".jpg") or filename.endswith(".png"):
-                image_file=os.path.join(path, filename)
-                frame = cv2.imread(image_file)
-#                cv2.imshow("Original", frame)
-                frame=image_processing.apply_image_transformation(frame)
-            else:
-                continue
-    except FileNotFoundError :
-        pass
+                else:
+                    continue
+        except FileNotFoundError :
+            pass
